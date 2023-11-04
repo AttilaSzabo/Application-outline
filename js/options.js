52303629b470c5bbdc56f2c75f310a212bb3ba15
Attila Szabó <lindi.2brothers@gmail.com>
2023-11-04 11:07:53 +0100

    Specifying Linditex options

--- js/options.js
+++ js/options.js
@@ -2,5 +2,5 @@ var linditex = document.querySelector(".linditex");
 
 
 linditex.addEventListener("click", () => {
-    window.location.assign("linditexwarehouse.html")
+    window.location.assign("linditexwarehouseoptions.html")
 });
